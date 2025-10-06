a18733a4fee3fa6725f70e8b4ecde0b38c7201b7
sinster23 <upayandutta204@gmail.com>
2025-10-06 18:19:23 +0530

    com

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use((req, res) => {
   res.status(404).json({ error: 'Endpoint not found' });
 });
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
 
 export default app;
