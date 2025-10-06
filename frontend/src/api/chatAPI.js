54dbc164adab2142704a20104e7c127653740eea
sinster23 <upayandutta204@gmail.com>
2025-10-06 20:10:25 +0530

    com

--- frontend/src/api/chatAPI.js
+++ frontend/src/api/chatAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/chat"; // your backend endpoint
+const API_URL = import.meta.env.VITE_API_URL; // your backend endpoint
 
 export async function sendMessageToBackend(message) {
   try {
